bae23e3dc2e506f78b61444a6928b04833cf2c9c
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-07-03 10:19:24 +0530

    post-commit2

--- index.js
+++ index.js
@@ -4,33 +4,69 @@ const app = express();
 app.use(express.json());
 
 let courses = [ 
-    {
-    id: 1,
-    name: 'course1'
-    } , {
-        id: 2,
-        name: 'course2'
-    } , {
-        id: 3,
-        name: 'course3'
-    } , ];
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+];
 
-    app.get('/courses',(req,res) => {
-        res.json(courses)
-    });
+// GET endpoint to retrieve all courses
+app.get('/courses', (req, res) => {
+    res.json(courses);
+});
 
-    app.post('/courses', (req, res) => {
-        const newCourse = req.body;
+// POST endpoint to add a new course
+app.post('/courses', (req, res) => {
+    //const newCourse = req.body;
     
-        if (!newCourse.id || !newCourse.name) {
-            res.status(400).send('Course must have an id and a name');
-        }
-    
-        courses.push(newCourse);
-        res.status(201).json(newCourse);
-    });
+    // if (!newCourse.id || !newCourse.name) {
+    //     return res.status(400).send('Course must have an id and a name');
+    // }
 
+    // courses.push(newCourse);
+    // res.status(201).json(newCourse);
+    console.log(req.body);
+    let singleCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(singleCourse);
+    res.send(courses);
+});
 
-    app.listen(3000,() => {
-        console.log('Listening on port 3000...')
-    })
+// PUT endpoint to update an existing course by id
+app.put('/courses/:id', (req, res) => {
+    const courseId = parseInt(req.params.id);
+    const updatedCourse = req.body;
+
+    const courseIndex = courses.findIndex(course => course.id === courseId);
+
+    if (courseIndex === -1) {
+        return res.status(404).send('Course not found');
+    }
+
+    if (!updatedCourse.name) {
+        return res.status(400).send('Course name is required');
+    }
+
+    courses[courseIndex].name = updatedCourse.name;
+    res.json(courses[courseIndex]);
+});
+
+// DELETE endpoint to delete an existing course by id
+app.delete('/courses/:id', (req, res) => {
+    const courseId = parseInt(req.params.id);
+
+    const courseIndex = courses.findIndex(course => course.id === courseId);
+
+    if (courseIndex === -1) {
+        return res.status(404).send('Course not found');
+    }
+
+    const deletedCourse = courses.splice(courseIndex, 1);
+    res.json(deletedCourse[0]);
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+});

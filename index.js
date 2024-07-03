e1877f8f7e2a7133adf701221cf5576456918479
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-07-03 10:07:26 +0530

    post-commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let courses = [ 
     {
     id: 1,
@@ -16,6 +18,18 @@ let courses = [
     app.get('/courses',(req,res) => {
         res.json(courses)
     });
+
+    app.post('/courses', (req, res) => {
+        const newCourse = req.body;
+    
+        if (!newCourse.id || !newCourse.name) {
+            res.status(400).send('Course must have an id and a name');
+        }
+    
+        courses.push(newCourse);
+        res.status(201).json(newCourse);
+    });
+
 
     app.listen(3000,() => {
         console.log('Listening on port 3000...')
